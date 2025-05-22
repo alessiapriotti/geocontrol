d50d470a12ed4d29de4990497b07e66427632a58
LUIGI COSTARELLA s345243 <s345243@studenti.polito.it>
2025-05-22 13:33:06 +0200

    spostati controlly gateway in checkService

--- src/controllers/gatewayController.ts
+++ src/controllers/gatewayController.ts
@@ -2,18 +2,22 @@ import { Gateway as GatewayDTO } from "@dto/Gateway";
 import { GatewayRepository } from "@repositories/GatewayRepository";
 import { NetworkRepository } from "@repositories/NetworkRepository";
 import { mapGatewayDAOToDTO } from "@services/mapperService";
+import {checkMacSensorsGateway,checkNetwork} from "@services/checkService";
 
 export async function createGateway(gatewayDTO: GatewayDTO, code: string): Promise<void> {
     
     const gatewayRep = new GatewayRepository();
-    const network = await (new NetworkRepository().getNetworkByCode(code));
+    const network = await(checkNetwork(code));
+    await(checkMacSensorsGateway(gatewayDTO.macAddress));
+    
     await gatewayRep.createGateway(gatewayDTO.macAddress, gatewayDTO.name, gatewayDTO.description, network);
 }
 
 export async function getAllGateway(networkCode: string): Promise<GatewayDTO[]> {
 
     const gatewayRep = new GatewayRepository();
-    await (new NetworkRepository().getNetworkByCode(networkCode));
+    await (checkNetwork(networkCode));
+
     return (await gatewayRep.getAllGateway(networkCode)).map(mapGatewayDAOToDTO);
 }
 
@@ -21,7 +25,8 @@ export async function getAllGateway(networkCode: string): Promise<GatewayDTO[]> 
 export async function getGatewayByMacAddress(networkCode: string, macAddress: string): Promise<GatewayDTO> {
 
     const gatewayRep = new GatewayRepository();
-    await (new NetworkRepository().getNetworkByCode(networkCode));
+    await (checkNetwork(networkCode));
+
     return mapGatewayDAOToDTO(await gatewayRep.getGatewayByMacAddress(networkCode, macAddress));
 }
 
@@ -32,9 +37,12 @@ export async function updateGateway(
 ): Promise<void> {
 
     const gatewayRep = new GatewayRepository();
-    await (new NetworkRepository().getNetworkByCode(networkCode));
+    await (checkNetwork(networkCode));
+    await (gatewayRep.getGatewayByMacAddress(networkCode, currentMacAddress));
 
-    await (gatewayRep.getGatewayByMacAddress(networkCode,currentMacAddress));
+    if ((gatewayDTO.macAddress!==undefined)&&(currentMacAddress !== gatewayDTO.macAddress)) {
+        await(checkMacSensorsGateway(gatewayDTO.macAddress));
+    }
 
     await gatewayRep.updateGateway(currentMacAddress, gatewayDTO.macAddress, gatewayDTO.name, gatewayDTO.description);
 }
@@ -42,6 +50,7 @@ export async function updateGateway(
 export async function deleteGateway(macAddress: string, networkCode: string): Promise<void> {
 
     const gatewayRep = new GatewayRepository();
-    await (new NetworkRepository().getNetworkByCode(networkCode));
+    await (checkNetwork(networkCode));
+
     await gatewayRep.deleteGateway(macAddress, networkCode);
 }
